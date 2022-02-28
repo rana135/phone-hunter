2c60490542b8140069d6f8f7f3caa4a274137f1c
rana135 <rana286090@gmail.com>
2022-03-01 02:37:40 +0600

    almost done

--- js/phone.js
+++ js/phone.js
@@ -23,9 +23,18 @@ const displayGadget = data =>{
         <div class="card" style="width: 18rem;">
   <img src="${phones.image}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${phones.phone_name}</h5>
-    <p class="card-text">${phones.brand}</p>
-    <button onclick="loadphoneBySlug('${phones.slug}')">Details</button>
+    <h5 class="card-title">
+    <strong> Phone Name:- </strong>
+    ${phones.phone_name}
+    </h5>
+
+    <p class="card-text">
+    <strong> Phone Brand:- </strong>
+    ${phones.brand}
+    </p>
+
+    <button onclick="loadphoneBySlug('${phones.slug}')">Details
+    </button>
 
   </div>
 </div>
@@ -41,19 +50,122 @@ const loadphoneBySlug = slug =>{
     .then(response => response.json())
     .then (data => displayPhoneDetail(data.data))
 }
-
+                                
 const displayPhoneDetail = phones =>{
     const phoneDetails = document.getElementById("phone-details");
+    phoneDetails.textContent= '';
+    // if(phones.releaseDate == ''){
+    //     alert ("found");
+    // }
     const makingDiv = document.createElement("div");
     makingDiv.classList.add("cssDesign");
     makingDiv.innerHTML =`
-    <div class="card" style="width: 18rem;">
-  <img src="${phones.image}" class="card-img-top" alt="...">
+    <div class="card" style="width: 30rem;">
+
+  <img class="w-50 mx-auto" src="${phones.image}" 
+  class="card-img-top" alt="...">
+
   <div class="card-body">
-    <h5 class="card-title">${phones.phone_name}</h5>
-    <p class="card-text">${phones.brand}</p>
+    <h5 class="card-title">
+    <strong> Phone Name:- </strong>
+    ${phones.name}
+    </h5>
+
+    <p class="card-text">
+    <strong> Release Date:- </strong>
+    ${phones.releaseDate}
+    </p>
+    
+    <p class="t-align"> <strong> <u> mainFeatures </u> </strong </p>
+
+     <p class="card-text">
+    <strong> chipSet:- </strong>
+    ${phones.mainFeatures.chipSet}
+    </p>
+
+    <p class="card-text">
+    <strong> Display Size:- </strong>
+    ${phones.mainFeatures.displaySize}
+    </p>
+
+    <p class="card-text">
+    <strong> memory:- </strong>
+    ${phones.mainFeatures.memory} 
+    </p>
+    
+    <p class="t-align"> <strong> <u> others </u> </strong </p>
+
+    <p class="card-text">
+    <strong> Bluetooth:- </strong>
+    ${phones.others.Bluetooth} 
+    </p>
+
+    <p class="card-text">
+    <strong> GPS:- </strong>
+    ${phones.others.GPS} 
+    </p>
+
+    <p class="card-text">
+    <strong> NFC:- </strong>
+    ${phones.others.NFC} 
+    </p>
+
+    <p class="card-text">
+    <strong> Radio:- </strong>
+    ${phones.others.Radio} 
+    </p>
+
+    <p class="card-text">
+    <strong> USB:- </strong>
+    ${phones.others.USB} 
+    </p>
+
+    <p class="card-text">
+    <strong> WLAN:- </strong>
+    ${phones.others.WLAN} 
+    </p>
+
+    <p class="t-align"> <strong> <u> Sensor </u> </strong </p>
+
+    <p class="card-text">
+    <strong> Sensors(0):- </strong>
+    ${phones.mainFeatures.sensors[0]} 
+    </p>
+
+    <p class="card-text">
+    <strong> Sensors(1):- </strong>
+    ${phones.mainFeatures.sensors[1]} 
+    </p>
+
+    <p class="card-text">
+    <strong> Sensors(2):- </strong>
+    ${phones.mainFeatures.sensors[2]} 
+    </p>
+
+    <p class="card-text">
+    <strong> Sensors(3):- </strong>
+    ${phones.mainFeatures.sensors[3]} 
+    </p>
+
+    <p class="card-text">
+    <strong> Sensors(4):- </strong>
+    ${phones.mainFeatures.sensors[4]} 
+    </p>
+    
+    <p class="card-text">
+    <strong> Sensors(5):- </strong>
+    ${phones.mainFeatures.sensors[5]} 
+    </p>
+
   </div>
 </div>
     `
     phoneDetails.appendChild(makingDiv);
 }
+
+
+
+
+
+
+
